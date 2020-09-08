5c783fb4a8571fa084b86ec4139aceb8159752c3
Антон Приходько <antprichod@hotmail.com>
2020-09-08 20:36:16 +0800

    Добавьте файлы проекта.

---
+++ PrintApp/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PrintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Печать срабатывает на Ctrl + Shift + S";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 54);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Автопринтер";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+    }
+}
+
---
+++ PrintApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly HookHelper _hook;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _hook = new HookHelper(
+                new[]
+                {
+                    Keys.S, 
+                    Keys.LControlKey, 
+                    Keys.LShiftKey
+                },
+                Execute, 
+                HookHelper.HowToRegister.KeyUp);
+        }
+        
+        private void Execute()
+        {
+            Print(GetPrintScreen());
+        }
+
+        public Bitmap GetPrintScreen()
+        {
+            Bitmap print = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(print);
+            graphics.CopyFromScreen(0, 0, 0, 0, print.Size);
+            graphics.Dispose();
+            return print;
+        }
+
+        public void Print(Bitmap printScreen)
+        {
+            printDialog1.Document = new PrintDocument {DefaultPageSettings = {Landscape = true, Margins = new Margins(20, 20, 20, 20)} };
+
+            
+            var defaultBounds = printDialog1.Document.DefaultPageSettings.Bounds;
+
+            var maxSize = Math.Max(printScreen.Width, printScreen.Height);
+            double mult = 4;
+
+            if (defaultBounds.Width > defaultBounds.Height)
+                defaultBounds = new Rectangle(0, 0, (int)(maxSize* mult), (int)(Math.Round((double)defaultBounds.Height / defaultBounds.Width * maxSize)* mult));
+            
+            if (defaultBounds.Width < defaultBounds.Height)
+                defaultBounds = new Rectangle(0, 0, (int)(Math.Round((double)defaultBounds.Width / defaultBounds.Height *maxSize)* mult), (int)(maxSize* mult));
+            
+
+            int psHeight = defaultBounds.Height;
+            int psWidth = (int)Math.Round(((double)printScreen.Size.Width) / printScreen.Size.Height * psHeight);
+            bool horizontalCentering = true; 
+
+            if (psWidth > defaultBounds.Width)
+            {
+                psWidth = defaultBounds.Width;
+                psHeight = (int)Math.Round((double)printScreen.Size.Height / printScreen.Size.Width * psWidth);
+                horizontalCentering = false;
+            }
+
+            Bitmap adaptedBitmap = new Bitmap(defaultBounds.Width,defaultBounds.Height);
+            Graphics graphics = Graphics.FromImage(adaptedBitmap);
+
+            graphics.DrawImage(printScreen,
+                horizontalCentering
+                    ? new Rectangle((defaultBounds.Width - psWidth) / 2, 0, psWidth, psHeight)
+                    : new Rectangle(0, (defaultBounds.Height - psHeight) / 2, psWidth, psHeight));
+
+            graphics.Dispose();
+
+
+            printDialog1.Document.PrintPage += (s, e) =>
+            {
+                e.Graphics.DrawImage(adaptedBitmap, e.MarginBounds);
+            };
+
+            
+            DialogResult result = printDialog1.ShowDialog();
+            
+            if (result == DialogResult.OK)
+                printDialog1.Document.Print();
+        }
+    }
+}
