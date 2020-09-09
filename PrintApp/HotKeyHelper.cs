c94fb14603e2dc8270ca6bb8811d03754394e257
Антон Приходько <antprichod@hotmail.com>
2020-09-09 10:48:56 +0800

    beta optimized

--- PrintApp/HotKeyHelper.cs
+++ PrintApp/HotKeyHelper.cs
@@ -84,32 +84,22 @@ namespace PrintApp
         [DllImport("user32.dll", SetLastError = true)]
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
-        private HashSet<Key> PressedKeys = new HashSet<Key>();
+        private readonly HashSet<Key> _pressedKeys = new HashSet<Key>();
 
-
-
-
-        /// <summary>
-        /// This functions scans currently pressed keys and returns them. Every key is returned just once. If the key is still pressed during second
-        /// method call, it is not returned. It's returned again after the key is released and pressed again.
-        /// </summary>
-        /// <returns>List of keys which were just pressed</returns>
         public List<Key> GetNewPressedKeys()
         {
             List<Key> newPressedKeys = new List<Key>(10);
 
-            // Get state of every key we know
             foreach (Key key in Utils.GetEnumValues<Key>().Where(x => x != Key.None))
             {
-                // Is it pressed?
                 bool down = Keyboard.IsKeyDown(key);
 
-                // It's not pressed, but it was - we consider this key as released
-                if (!down && PressedKeys.Contains(key))
-                    PressedKeys.Remove(key);
-                else if (down && !PressedKeys.Contains(key)) // The key is pressed, but wasn't pressed before - it will be returned
+                if (!down && _pressedKeys.Contains(key))
+                    _pressedKeys.Remove(key);
+
+                else if (down && !_pressedKeys.Contains(key)) // The key is pressed, but wasn't pressed before - it will be returned
                 {
-                    PressedKeys.Add(key);
+                    _pressedKeys.Add(key);
                     newPressedKeys.Add(key);
                 }
             }
@@ -120,11 +110,6 @@ namespace PrintApp
 
     public static class Utils
     {
-        /// <summary>
-        /// Gets all values of given enum
-        /// </summary>
-        /// <typeparam name="T">Enum whose values we want</typeparam>
-        /// <returns>Values of the enum</returns>
         public static IEnumerable<T> GetEnumValues<T>()
         {
             return Enum.GetValues(typeof(T)).Cast<T>();
